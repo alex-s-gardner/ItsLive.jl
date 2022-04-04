096e6c8521236a12180896e760c691c410db9d38
Alex Gardner <32276930+alex-s-gardner@users.noreply.github.com>
2022-04-04 13:44:01 -0700

    Update save2h5.jl

--- src/datacube/save2h5.jl
+++ src/datacube/save2h5.jl
@@ -15,14 +15,37 @@ Alex S. Gardner, JPL, Caltech.
 
 
 function save2h5(C, outfilename)
-# save data to a local HDF5
-# outfilename = "/Users/XXX/Desktop/ITS_LIVE_output.h5"
-h5open(outfilename, "w")
-varnames = names(C)[2];
-if ndims(C) == 2
-    for pt = 1:size(C,1)
+    # save data to a local HDF5
+    # outfilename = "/Users/XXX/Desktop/ITS_LIVE_output.h5"
+    h5open(outfilename, "w")
+    varnames = names(C)[2];
+    if ndims(C) == 2
+        for pt = 1:size(C,1)
+            for vname in varnames
+                v = C[pt,vname]
+                if v isa Vector{Union{Missing, Float64}}
+                    v = Float64.(replace(v, missing => NaN))
+                elseif vname == "satellite_img1" .|| vname == "satellite_img2"
+                    id, sensorgroups = ItsLive.sensorgroup(v)
+                    v = id;
+                    sensor_info = "";
+                    for i = 1:length(sensorgroups)
+                        s = sensorgroups[i]
+                        if i == length(sensorgroups)
+                            sensor_info = sensor_info * repr(s["id"]) * "=" * s["name"]
+                        else
+                            sensor_info = sensor_info * repr(s["id"]) * "=" * s["name"] * ", " 
+                        end
+                    end
+                    
+                    write(outfilename, vname*"_info", sensor_info)
+                end
+                write(outfilename, vname * string(pt), v)
+            end
+        end
+    else
         for vname in varnames
-            v = C[pt,vname]
+            v = C[vname]
             if v isa Vector{Union{Missing, Float64}}
                 v = Float64.(replace(v, missing => NaN))
             elseif vname == "satellite_img1" .|| vname == "satellite_img2"
@@ -40,29 +63,7 @@ if ndims(C) == 2
                 
                 write(outfilename, vname*"_info", sensor_info)
             end
-            write(outfilename, vname * string(pt), v)
+            write(outfilename, vname * pt, v)
         end
-    end
-else
-    for vname in varnames
-        v = C[vname]
-        if v isa Vector{Union{Missing, Float64}}
-            v = Float64.(replace(v, missing => NaN))
-        elseif vname == "satellite_img1" .|| vname == "satellite_img2"
-            id, sensorgroups = ItsLive.sensorgroup(v)
-            v = id;
-            sensor_info = "";
-            for i = 1:length(sensorgroups)
-                s = sensorgroups[i]
-                if i == length(sensorgroups)
-                    sensor_info = sensor_info * repr(s["id"]) * "=" * s["name"]
-                else
-                    sensor_info = sensor_info * repr(s["id"]) * "=" * s["name"] * ", " 
-                end
-            end
-            
-            write(outfilename, vname*"_info", sensor_info)
-        end
-        write(outfilename, vname * pt, v)
     end
 end
