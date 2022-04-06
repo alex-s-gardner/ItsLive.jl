6c59b293f6da506f78e3a2fc1de8a1cd3fe6524b
Alex Gardner <32276930+alex-s-gardner@users.noreply.github.com>
2022-04-06 09:54:30 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,13 +8,6 @@ makedocs(
     authors="Alex S. Gardner, JPL, Caltech",
     repo="https://github.com/alex-s-gardner/ItsLive.jl/blob/{commit}{path}#{line}",
     sitename = "ItsLive.jl",
-    format=Documenter.HTML(;
-        canonical="https://alex.s.gardner.github.io/ItsLive.jl/dev/",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],  
 )
 
 deploydocs(
