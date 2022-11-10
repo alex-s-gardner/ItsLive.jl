d42ededb7b1dbd7e44d61ba14961bc21148deed3
Alex Gardner <32276930+alex-s-gardner@users.noreply.github.com>
2022-11-09 21:27:04 -0800

    Update nearestxy.jl

--- src/datacube/nearestxy.jl
+++ src/datacube/nearestxy.jl
@@ -14,9 +14,9 @@ julia> nearestxy(lat,lon,dc)
 # Arguments
    - `lat::::Union{Vector,Number}`: latitude between -90 and 90 degrees
    - `lon::::Union{Vector,Number}`: latitude between -180 and 180 degrees
-   - `dc:::ZGroup{Zarr.ConsolidatedStore{Zarr.HTTPStore}}`: ITS_LIVE data cube:: Zarr DataArray 
+   - `dc:::Union{ZGroup{Zarr.ConsolidatedStore{Zarr.HTTPStore}}, ZGroup{S3Store}}`: ITS_LIVE data cube:: Zarr DataArray 
 """
-function nearestxy(lat::Union{Vector,Number}, lon::Union{Vector,Number}, dc::Union{ZGroup{Zarr.ConsolidatedStore{Zarr.HTTPStore}}, ZGroup{S3Store}})
+function nearestxy(lat::Union{Vector,Number}, lon::Union{Vector,Number}, dc::Union{ZGroup{Zarr.ConsolidatedStore{Zarr.HTTPStore}}, ZGroup{S3Store}, ZGroup{Zarr.ConsolidatedStore{Zarr.ConsolidatedStore{Zarr.HTTPStore}}}})
   # check that lat is within range
   if any(lat .< -90) || any(lat .> 90)
     error("lat = $lat, not in range [-90 to 90]")
